bc2c22bf1a54f7b7533aaf2b0559fa650cb88679
Patrick Gavigan <pgavigan@gmail.com>
2020-01-16 13:57:46 -0500

    Started working on the bash script

---
+++ resources/configProject.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Sample configuration script for projects that use savi_ros_bdi.
+
+# Set the location of savi_ros_bdi here
+SAVI_ROS_DIR = ~/SAVI_ROS/rosjavaWorkspace/src/savi_ros_java/savi_ros_bdi/
+
+# Set the location of this project
+PROJECT_DIR = pwd
+
+echo SAVI_ROS_DIR
+exho PROJECT_DIR
+
+#/build/instl/savi_ros_bdi/bin
